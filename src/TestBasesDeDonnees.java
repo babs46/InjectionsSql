c847ebe188cb7278d95de045560a03b247b2f79f
Baba Mamadou Camara <77743529+babs46@users.noreply.github.com>
2023-08-30 21:35:26 -0400

    Update TestBasesDeDonnees.java

--- src/TestBasesDeDonnees.java
+++ src/TestBasesDeDonnees.java
@@ -1,15 +1,20 @@
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-
+import org.apache.log4j.PropertyConfigurator;
 
 public class TestBasesDeDonnees {
-   private static Logger logger = LogManager.getLogger(TestBasesDeDonnees.class);
+    private static final Logger mysqlLogger = Logger.getLogger("mysqlLogger");
+    private static final Logger postgresqlLogger = Logger.getLogger("postgresqlLogger");
 
     // Configuration de la connexion aux bases de données
     private static final String MYSQL_DB_URL = "jdbc:mysql://localhost:3306/mysql_test";
@@ -21,56 +26,58 @@ public class TestBasesDeDonnees {
     private static final String POSTGRES_DB_PASSWORD = "1234";
 
     public static void main(String[] args) {
-        // Génération des requêtes de test pour les différentes bases de données
-        List<String> mysqlQueries = generateMySQLTestQueries();
-        List<String> postgresQueries = generatePostgresTestQueries();
+
+        // Génération des requêtes de test à partir des fichiers texte
+        List<String> mysqlQueries = generateTestQueries(mysqlLogger,"C:\\Users\\User\\IdeaProjects\\InjectionsSql\\src\\mysql_test_inputs.txt");
+        List<String> postgresQueries = generateTestQueries(postgresqlLogger,"C:\\Users\\User\\IdeaProjects\\InjectionsSql\\src\\postgres_test_inputs.txt");
+
+        // Configuration pour MySQL
+        PropertyConfigurator.configure("log4j_mysql.properties");
+        Logger mysqlLogger = Logger.getLogger("mysqlLogger");
+
+        // Configuration pour PostgreSQL
+        PropertyConfigurator.configure("log4j_postgresql.properties");
+        Logger postgresqlLogger = Logger.getLogger("postgresqlLogger");
 
         // Connexion à la base de données MySQL et exécution des requêtes de test
         try (Connection mysqlConnection = DriverManager.getConnection(MYSQL_DB_URL, MYSQL_DB_USER, MYSQL_DB_PASSWORD)) {
-            logger.info("Connexion réussie à la base de données MySQL !");
-            testInputs(mysqlConnection, mysqlQueries);
+            mysqlLogger.info("Connexion réussie à la base de données MySQL !");
+            testInputs(mysqlConnection, mysqlQueries,mysqlLogger);
         } catch (SQLException e) {
-            logger.error("Erreur lors de la connexion à la base de données MySQL : " + e.getMessage()+"\n");
+            mysqlLogger.error("Erreur lors de la connexion à la base de données MySQL : " + e.getMessage());
             e.printStackTrace();
         }
 
         // Connexion à la base de données PostgreSQL et exécution des requêtes de test
         try (Connection postgresConnection = DriverManager.getConnection(POSTGRES_DB_URL, POSTGRES_DB_USER, POSTGRES_DB_PASSWORD)) {
-            logger.info("Connexion réussie à la base de données PostgreSQL !");
-            testInputs(postgresConnection, postgresQueries);
+            postgresqlLogger.info("Connexion réussie à la base de données PostgreSQL !");
+            testInputs(postgresConnection, postgresQueries,postgresqlLogger);
         } catch (SQLException e) {
-            logger.error("Erreur lors de la connexion à la base de données PostgreSQL : " + e.getMessage());
+            postgresqlLogger.error("Erreur lors de la connexion à la base de données PostgreSQL : " + e.getMessage());
             e.printStackTrace();
         }
     }
 
-    // Méthode pour générer les requêtes de test pour la base de données MySQL
-    private static List<String> generateMySQLTestQueries() {
+    // Méthode pour générer les requêtes de test à partir d'un fichier texte
+    private static List<String> generateTestQueries(Logger logger,String filePath) {
         List<String> queries = new ArrayList<>();
 
-        //queries.add ("SELECT * FROM album WHERE Title = '' OR 1=1 --");
-       // queries.add ("SELECT 1/0; OR 1=1;  --");
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                queries.add(line);
+            }
+        } catch (IOException e) {
+            logger.error("Erreur lors de la lecture du fichier : " + e.getMessage());
 
-        //queries.add("SELECT* FROM artist where ArtistId=1;drop table genre");
-       // queries.add ("' UNION SELECT null, Username, Password FROM useraccount; --");
+            e.printStackTrace();
+        }
 
-
-        return queries;
-    }
-
-    // Méthode pour générer les requêtes de test pour la base de données PostgreSQL
-    private static List<String> generatePostgresTestQueries() {
-        List<String> queries = new ArrayList<>();
-        //queries.add ("SELECT * FROM album WHERE \"Title\" = '' OR 1=1 --");
-       // queries.add (" SELECT 1/0;  OR 1=1; --");
-
-        //queries.add (" UNION SELECT null, \"Username\", \"Password\" FROM useraccount; --");
-       // queries.add("SELECT* FROM artist where \"ArtistId\" =1;drop table genre");
         return queries;
     }
 
     // Méthode pour tester les requêtes et récupérer les données
-    private static void testInputs(Connection connection, List<String> queries) {
+    private static void testInputs(Connection connection, List<String> queries,Logger logger) {
         for (String query : queries) {
             try (PreparedStatement pstmt = connection.prepareStatement(query)) {
                 if (query.trim().toUpperCase().startsWith("SELECT")) {
@@ -102,16 +109,18 @@ public class TestBasesDeDonnees {
                         }
 
                         // Afficher une ligne de séparation
-                        logger.info("-".repeat(rowData.length() + 4));
+                        System.out.print("-".repeat(rowData.length() + 4));
                     }
                 } else {
                     // Pour les autres types de requêtes (INSERT, UPDATE, DELETE), nous utilisons executeUpdate
                     int rowsAffected = pstmt.executeUpdate();
                     logger.info("Requête exécutée : " + query + ". Nombre de lignes affectées : " + rowsAffected);
+
                 }
             } catch (SQLException e) {
                 logger.error("Erreur lors de l'exécution de la requête : " + query + "\nErreur : " + e.getMessage()+"\n");
                 e.printStackTrace();
+
             }
         }
     }
